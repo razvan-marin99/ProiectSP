8850e84c38ab2bd0ef4f716569296c06cc50147e
razvan-marin99 <56434122+razvan-marin99@users.noreply.github.com>
2019-10-31 18:08:02 +0200

    Update T1_ex4_Razvan_Marin.m

--- T1_ex4_Razvan_Marin.m
+++ T1_ex4_Razvan_Marin.m
@@ -1,4 +1,14 @@
-t=0:0.002:10;
-f=2;
-f = @(t) 0.8*sin(t).*(sin(t)>=0) + 0*(sin(t)<0); %semnalul are valoarea normala pe partea pozitiva, zero cand ar trebui sa ia valori negative
-plot(t,f(t))
+t1=0:0.002:10;
+t2=0:0.02:10;
+t3=0:0.2:10;
+amp = 0.8;
+f=0.3333;
+sig1=amp*sin(2*pi*f*t1);
+sig2=amp*sin(2*pi*f*t2);
+sig3=amp*sin(2*pi*f*t3);
+for i=1:length(sig1)
+    if sig1(i)<0
+        sig1(i)=0;
+    end
+end
+plot(t1,sig1)
